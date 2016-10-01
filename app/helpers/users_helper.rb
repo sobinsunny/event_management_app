c4217404bb866eb1c25cf4235715d7d2e1f66661
Sobin <sobisunny@gmail.com>
2016-10-01 16:24:27 +0530

    added conformation helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,7 @@ module UsersHelper
 	def gender_lists 
 		I18n.t(:gender_lists).map { |key, value| [ value, key ] } 
 	end
+	def is_confirmed(event)
+		event.user_events.where(user_id:current_user.id)
+	end
 end
