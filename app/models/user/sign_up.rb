74a58e798ccb7ecc58e7cf8f4e5ad404ab631dd7
Sobin <sobisunny@gmail.com>
2016-10-01 15:08:58 +0530

    user events

--- app/models/user/sign_up.rb
+++ app/models/user/sign_up.rb
@@ -1,6 +1,6 @@
 class User::SignUp < ActiveType::Record[User]
   validates :password, confirmation: true
-  validates :email, :password, :password_confirmation, :name, presence: true
+  validates :email, :password, :password_confirmation, presence: true
   validates :password, length: { minimum: 6 }
   before_save :encrypt_password
   after_save :clear_password
