686da95abbc673af56c3387a3a84c5821aece766
Sobin <sobisunny@gmail.com>
2016-10-06 14:19:57 +0530

    Update form_helper.rb

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,7 +1,7 @@
 module FormHelper
   # custom error message
   def error_message(form, feild)
-    if form.object.errors[feild].present?
+    if form.object.errors[field].present?
       error_messsage =  form.object.errors[feild].try(:first).to_s
       content_tag(:div, error_messsage, class: 'help-inline')
     end
